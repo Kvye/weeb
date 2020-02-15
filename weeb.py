8dcf607ab7a54d6f8c5fd3934a410ab1869e63ad
Kvye thinkpad <Karlallanjohnvye@gmail.com>
2020-02-15 22:54:44 +0000

    Updated our functionality to fit in the Query methods
    reformatted the functionality to be inside of our methods so they are ready to be used

--- weeb.py
+++ weeb.py
@@ -2,56 +2,45 @@ from jikanpy import Jikan
 jikan = Jikan()
 
 class Query:
+    """Searches for the anime being queried"""
     def __init__(self, name):
-        pass
+        anime = jikan.search('anime', name) # searches for anime with the name provided
+        self.searchresults = anime.get("results") # get the results only
+        self.sortedlist = [] # empty list to be used for sorting
+        self.specify = "null" # string to be used for our final specified result
+
+    """Sorts and returns our anime search results based on a property and in descending order or not"""
     def sort(self, sort_by = "score", descending = False):
-        pass
+        for x in self.searchresults: # go through all the results
+            self.sortedlist.append(x[sort_by]) # append to a list every instance of the property we are using
+        self.sortedlist.sort(reverse=descending) # sort that list to sort our results
+        for x in self.searchresults: # go through all the results again
+            if x[sort_by] == self.sortedlist[0]: # if current index is same as our sorted list
+                self.specify = x # then that index is the sorted result we want
+                anime_name = "name: " + self.specify["title"] 
+                anime_image = "image: " + self.specify["image_url"]
+                anime_episodes = "episodes: " + str(self.specify["episodes"])
+                anime_score = "score: " + str(self.specify["score"])
+                anime_synopsis = "synopsis: " + self.specify["synopsis"]
+                animeresult = anime_name + anime_image + anime_episodes + anime_score + anime_synopsis
+                # obtain only the information we need from our sorted search result
+                return animeresult                
+
+    """Returns our top/first anime result or one we specify with an index"""
     def get_anime(self, which = 0):
-        pass
+        self.specify = self.searchresults[which] # obtain the search result index
+        anime_name = "name: " + self.specify["title"]
+        anime_image = "image: " + self.specify["image_url"]
+        anime_episodes = "episodes: " + str(self.specify["episodes"])
+        anime_score = "score: " + str(self.specify["score"])
+        anime_synopsis = "synopsis: " + self.specify["synopsis"]
+        animeresult = anime_name + anime_image + anime_episodes + anime_score + anime_synopsis
+        # obtain only the information we need from our search result
+        return animeresult
 
-
-obtainmal = jikan.search('anime', 'Naruto') # search MAL for an anime on page 1
-searchresults = obtainmal.get("results") # get only the results
-sortedlist = [] # create a list for sorting
-for x in searchresults:
-    sortedlist.append(x["title"]) # append the sorting criteria to the sorting list
-sortedlist.sort(reverse=True) # sort the list, index [0] will be considered the sorted result
-for x in searchresults: # go through the search results
-    if x["title"] == sortedlist[0]: # look for the result based on what is considered sorted
-        specify = x # the sorted result is now our specified/final result to use
-"""searches through MAL and sorts the result based on a property"""
-
-anime_name = specify["title"] # get anime name
-anime_image = specify["image_url"] # get anime image url
-anime_episodes = specify["episodes"] # get anime episodes
-anime_score = specify["score"] # get anime score
-anime_synopsis = specify["synopsis"] # get anime synopsis
-print("Name: " + anime_name,
-        "Url:" + anime_image,
-        "Episodes: " + str(anime_episodes), 
-        "Score: " + str(anime_score), 
-        "Synopsis: " + anime_synopsis
-) # print name, image, episodes, score, synopsis for what we searched
-
-obtainmal = jikan.search('anime', 'Naruto') # search MAL for an anime on page 1
-searchresults = obtainmal.get("results") # get only the results
-specify = searchresults[0] # take the first result only
-
-anime_name = specify["title"] # get anime name
-anime_image = specify["image_url"] # get anime image url
-anime_episodes = specify["episodes"] # get anime episodes
-anime_score = specify["score"] # get anime score
-anime_synopsis = specify["synopsis"] # get anime synopsis
-print("Name: " + anime_name,
-        "Url:" + anime_image,
-        "Episodes: " + str(anime_episodes), 
-        "Score: " + str(anime_score), 
-        "Synopsis: " + anime_synopsis
-) # print name, image, episodes, score, synopsis for what we searched
-"""searches through MAL for an anime and gives the title of the first result"""
-
-
-
+q = Query("Naruto") # q is a search for "Naruto" 
+print(q.sort()) # print out a sorted search result
+print(q.get_anime()) # print out a search result
 
 if __name__ == '__main__':
     pass

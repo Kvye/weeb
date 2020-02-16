748f4da3b2f1da29586027a12061025e277147e1
Kvye thinkpad <Karlallanjohnvye@gmail.com>
2020-02-16 18:10:10 +0000

    Added functionality that allows searches through command line arguments
    filled in the Main Function to include command line arguments and to use these arguments for searching.
    Updated requirements.txt to include argparse

--- weeb.py
+++ weeb.py
@@ -1,4 +1,5 @@
 from jikanpy import Jikan
+import argparse
 jikan = Jikan()
 
 class Query:
@@ -30,17 +31,35 @@ class Query:
     def get_anime(self, which = 0):
         self.specify = self.searchresults[which] # obtain the search result index
         anime_name = "name: " + self.specify["title"]
-        anime_image = "image: " + self.specify["image_url"]
-        anime_episodes = "episodes: " + str(self.specify["episodes"])
-        anime_score = "score: " + str(self.specify["score"])
-        anime_synopsis = "synopsis: " + self.specify["synopsis"]
+        anime_image = " image: " + self.specify["image_url"]
+        anime_episodes = " episodes: " + str(self.specify["episodes"])
+        anime_score = " score: " + str(self.specify["score"])
+        anime_synopsis = " synopsis: " + self.specify["synopsis"]
         animeresult = anime_name + anime_image + anime_episodes + anime_score + anime_synopsis
         # obtain only the information we need from our search result
         return animeresult
 
-q = Query("Naruto") # q is a search for "Naruto" 
-print(q.sort()) # print out a sorted search result
-print(q.get_anime()) # print out a search result
-
 if __name__ == '__main__':
-    pass
+    """add arguments for parsing"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("animetitle", help="search for this anime") # add anime title argument
+    parser.add_argument("--property", help="sort with this property") # add property argument
+    parser.add_argument("--desc", default=False, help="sort by descending or not True or False") # add descending argument
+    args = parser.parse_args()
+
+    """convert the true/false strings to booleans for the sort method"""
+    boolcheck = args.desc # get our desc parameter to check if they wrote true or false
+    if args.desc != False: # if the desc parameter was used
+        boolcheck = args.desc.upper() # convert descending to uppercase so both True and true work etc
+    if boolcheck == "TRUE": 
+        args.desc = True # if user enters true the argument becomes the bool True
+    elif boolcheck == "FALSE":
+        args.desc = False # if user enters false the argument becomes the bool False
+    q = Query(args.animetitle) # q is a search for what is entered in the animetitle parameter
+    
+    """run a sorted search if sort property is specified otherwise run a regular search"""
+    if args.property == None: # if a sort property hasnt been specified
+        print(q.get_anime()) # print out a search result
+    else:
+        print(q.sort(args.property, args.desc)) # print out a sorted search result
+    
